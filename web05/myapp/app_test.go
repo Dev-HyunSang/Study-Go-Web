66c824cc2cfab48f027e3ebc010cb4c15bd8b551
HyunSang Park <hyun.sang@parkhyunsang.com>
2021-06-22 09:45:33 +0900

    [UPDATE] API 3 item Development

--- web05/myapp/app_test.go
+++ web05/myapp/app_test.go
@@ -18,8 +18,8 @@ func TestIndex(t *testing.T) {
 	resp, err := http.Get(ts.URL)
 	assert.NoError(err)
 	assert.Equal(http.StatusOK, resp.StatusCode)
-
 	data, _ := ioutil.ReadAll(resp.Body)
+	assert.Equal("Hello World", string(data))
 }
 
 func TestUsers(t *testing.T) {
@@ -31,7 +31,25 @@ func TestUsers(t *testing.T) {
 	resp, err := http.Get(ts.URL + "/users")
 	assert.NoError(err)
 	assert.Equal(http.StatusOK, resp.StatusCode)
-
 	data, _ := ioutil.ReadAll(resp.Body)
 	assert.Contains(string(data), "Get UserInfo")
+}
+
+func TestGetUserInfo(t *testing.T) {
+	assert := assert.New(t)
+
+	ts := httptest.NewServer(NewHandler())
+	defer ts.Close()
+
+	resp, err := http.Get(ts.URL + "/users/89")
+	assert.NoError(err)
+	assert.Equal(http.StatusOK, resp.StatusCode)
+	data, _ := ioutil.ReadAll(resp.Body)
+	assert.Contains(string(data), "User Id:89")
+
+	resp, err = http.Get(ts.URL + "/users/56")
+	assert.NoError(err)
+	assert.Equal(http.StatusOK, resp.StatusCode)
+	data, _ = ioutil.ReadAll(resp.Body)
+	assert.Contains(string(data), "User Id:56")
 }
